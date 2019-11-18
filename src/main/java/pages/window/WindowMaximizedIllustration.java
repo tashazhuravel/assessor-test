0c47e979df83edb04463b1028525937248eb9f2d
N.Ilyushenko@isida.by <50950562+tashazhuravel@users.noreply.github.com>
2019-11-18 15:15:40 +0300

    WindowMaximizedIllustration

--- src/main/java/pages/window/WindowMaximizedIllustration.java
+++ src/main/java/pages/window/WindowMaximizedIllustration.java
@@ -2,23 +2,54 @@ package pages.window;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 import pages.IllustrationsPage;
 
 public class WindowMaximizedIllustration extends IllustrationsPage {
 
+
     private By headerMaximizedIllustration = By.xpath("//div[@id='windowIllustration']//span/center");
 
+    Actions actions;
 
     public WindowMaximizedIllustration(WebDriver webDriver) {
         super(webDriver);
     }
 
-   public IllustrationsPage clickTabFIOParticipant(){
+
+    public IllustrationsPage clickTabFIOParticipant() {
         usersFIOTab.iterator().next().click();
         return this;
-   }
+    }
+
+    public String getSubjectSelectedQuestionText() {
+        return subjectSelectedQuestion.getAttribute("value");
+    }
+
+    public IllustrationsPage clickSlideshowButton() {
+        actions.moveToElement(slideshowButton).click().perform();
+        return this;
+    }
+
+    public IllustrationsPage clickDeleteImageButton() {
+        actions.moveToElement(deleteImageButton).click().perform();
+        return this;
+    }
+
+    public IllustrationsPage clickImagePageButton() {
+        actions.moveToElement(imagePageButton).click().perform();
+        return this;
+    }
+
+    public String getImagePageNumber() {
+        return imagePageNumber.getText();
+    }
+
+    public WebElement getImageContent() {
+        return imageContent;
+    }
 
 
 }
