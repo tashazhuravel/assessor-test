e72204d8a7b98be33260e6306dece848525e0787
N.Ilyushenko@isida.by <50950562+tashazhuravel@users.noreply.github.com>
2019-11-20 17:40:12 +0300

    StatementPage

--- src/main/java/pages/StatementPage.java
+++ src/main/java/pages/StatementPage.java
@@ -1,13 +1,34 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class StatementPage {
 
     private WebDriver webDriver;
     private Actions actions;
+
+    private By headerStatement = By.xpath("(//div[@id='viewPanel']//span)[1]");
+
+    @FindBy(xpath = "//table[@id='excerptDownloadBtn']//button")
+    private WebElement downloadThisText;
+
+    @FindBy(xpath = "//table[@id='excerptUploadBtn']//button")
+    private WebElement uploadEditedTextButton;
+
+    @FindBy(xpath = "//table[@id='agendaEditBtn']//button")
+    private WebElement editInWordButton;
+
+    @FindBy(xpath = "//table[@id='refreshReportBtn']//button")
+    private WebElement refreshStatement;
+
+    @FindBy(xpath = "//div[@class='textLayer']/div")
+    private WebElement textFromProtocol;
+
 
     public StatementPage(WebDriver webDriver){
         this.webDriver = webDriver;
